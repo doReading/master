cda64f41ce13b158533e065b8cfa8f182ae14e55
wanghuiguang <wanghuiguang@niwodai.net>
2015-10-26 10:40:06 +0800

    新建项目

---
+++ DoReading/RootViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DoReading
+//
+//  Created by Wang Huiguang on 15/10/26.
+//  Copyright © 2015年 ForHappy. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
